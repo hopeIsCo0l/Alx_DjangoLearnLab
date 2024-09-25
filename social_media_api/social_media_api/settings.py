e849ecd6f0e8e833aca90bdba02e3a8509b138b5
tsion <tsionnegash>
2024-09-25 21:30:27 +0300

    updating setting

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'social-media',
         'USERNAME': 'root',
-        'PASSWORD': 'Eyob@611730',
+        'PASSWORD': 'tsion@1234',
         'HOST': 'localhost',
         'PORT': '3306'
     }
