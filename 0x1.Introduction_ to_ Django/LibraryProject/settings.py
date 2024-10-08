830a0cd206356279e8e711ee4dedac2f4ebf4365
Amanuel <amanuel.ayalew@aait.edu.et>
2024-10-08 13:25:10 +0300

    check1

--- 0x1.Introduction_ to_ Django/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/ 
+#testing commit
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-d#!-)urzvfik=-+#0@hbho$1mtn7!vvkjt(q-o6e!)61h@cw0-'
